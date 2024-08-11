542c999b5be0ed07df53bd9d81d4a0d8bf1f3479
Данила Гаврютенков <pogranichnik.work@yandex.ru>
2024-08-12 00:46:57 +0300

    Написан метод move.

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ function shiftArray(array, value = 0) {
   let oldArray = array;
   let newArray = [];
 
-  if (Math.abs(value) >= array.length) return oldArray;
+  if (!(value % oldArray.length) || !value) return oldArray;
 
   for (let i = 0; i < oldArray.length; i++) {    
-    if (i + value >= oldArray.length) newArray[i + value - oldArray.length] = oldArray[i];
-    else if (i + value < 0) newArray[oldArray.length - i + value] = oldArray[i];
-    else {
-      newArray[i + value] = oldArray[i];
+    if (value < 0 ) {
+      if (value + i < 0) {
+        newArray[oldArray.length + value] = oldArray[i];
+      }
+      newArray[(i - value) % oldArray.length] = oldArray[i];
     }
+    else newArray[(i + value) % oldArray.length] = oldArray[i];
   }
 
   return newArray;
@@ -40,6 +42,14 @@ class MultiSlider {
   #init() {
     this.featuresOptions.forEach(value => value.ending ??= "");
 
+    for (let i = 0; i < this.slides.length; i++) {
+      if (this.slides[i].nodeName === "IMG") {
+        this.baseImages.push(this.slides[i].getAttribute("src"));
+      } else {
+        this.baseImages.push(getComputedStyle(this.slides[i]).getPropertyValue("background-image"));
+      }
+    }
+
     // Установка свойств из объекта option
     for (let i = 0; i < this.slides.length; i++) {
       this.featuresOptions.forEach(value => {        
@@ -50,5 +60,18 @@ class MultiSlider {
     // Инициализация главного слайда
     if (!this.options.mainSlide) this.mainSlide = Math.round(this.slides.length / 2);
     else this.mainSlide = this.options.mainSlide;
+  }
+
+  #move(number) {
+    let value = this.mainSlide - number;
+    this.baseImages = shiftArray(this.baseImages, value);
+
+    for (let j = 0; j < this.slides.length; j++) {
+      if (this.slides[j].nodeName === "IMG") {        
+        this.slides[j].setAttribute("src", this.baseImages[j]);
+      } else {
+        this.slides[j].style.backgroundImage = this.baseImages[number];
+      }
+    }
   }
 }
